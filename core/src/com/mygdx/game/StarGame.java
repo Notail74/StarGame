8c3f2762be20c4f3cdebc2d1ed63952f514a6acf
Protsenko Maksim <net3311971@icloud.com>
2019-11-04 22:21:38 +0700

    DZ 1

--- core/src/com/mygdx/game/StarGame.java
+++ core/src/com/mygdx/game/StarGame.java
@@ -10,25 +10,25 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class StarGame extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture img;
-	private TextureRegion region;
+	//private TextureRegion region;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 20, 30, 50, 70);
+		img = new Texture("Background.jpg");
+		//region = new TextureRegion(img, 20, 30, 50, 70);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0.255f, 0.11f, 0.19f, 0.2f);//цвет фона
+		Gdx.gl.glClearColor(1f, 1f, 1f, 1f);//цвет фона
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();// метод передачи текстур
 		batch.setColor(1f, 1f, 1f, 1);//метод отрисовки картинки
-		batch.draw(img, 100, 50, 100, 100);// метод для отрисовки
-		batch.draw(region, 400, 400);
-		batch.setColor(1f, 1f, 1f, 0.2f);
-		batch.draw(img, 100, 50);
+		batch.draw(img, 280, 200, 250, 250);// метод для отрисовки
+		//batch.draw(region, 400, 400);
+		//batch.setColor(1f, 1f, 1f, 0.2f);
+		//batch.draw(img, 100, 50);
 		batch.end(); // закрытие передачи текстур
 	}
 	
