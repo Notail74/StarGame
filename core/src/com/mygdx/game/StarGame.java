441a717fc882684c6f74ecf28b494781f352abaf
Protsenko Maksim <net3311971@icloud.com>
2019-11-04 22:28:01 +0700

    no com

--- core/src/com/mygdx/game/StarGame.java
+++ core/src/com/mygdx/game/StarGame.java
@@ -10,7 +10,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class StarGame extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture img;
-	//private TextureRegion region;
+	private TextureRegion region;
 
 	@Override
 	public void create () {
